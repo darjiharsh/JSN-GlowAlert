d3c2e975b03402cadcfaf6814234252d90cabfef
Harsh <harshdarji82@gmail.com>
2025-08-23 19:57:38 -0400

    Render PHTML using ifconfig and added backend model to serialize the array

---
+++ Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+namespace JSN\GlowAlert\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+    const XML_PATH_ENABLED = 'jsn_glowalert/general/enabled';
+
+    public function isEnabled()
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_ENABLED, ScopeInterface::SCOPE_STORE);
+    }
+}
+?>
