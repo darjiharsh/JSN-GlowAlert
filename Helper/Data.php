fe1111df0a0351c2b33d44bbe840e6d6047bf714
Harsh <harshdarji82@gmail.com>
2025-08-24 18:14:55 -0400

    Created helper function to get data into phtml file and init js based condition

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,10 +7,20 @@ use Magento\Store\Model\ScopeInterface;
 class Data extends AbstractHelper
 {
     const XML_PATH_ENABLED = 'jsn_glowalert/general/enabled';
+        const XML_PATH_ALERT_LOCATIONS = 'jsn_glowalert/general/alert_locations';
 
     public function isEnabled()
     {
         return $this->scopeConfig->isSetFlag(self::XML_PATH_ENABLED, ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getAlertLocations()
+    {
+        $locations = $this->scopeConfig->getValue(self::XML_PATH_ALERT_LOCATIONS, ScopeInterface::SCOPE_STORE);
+        $locationsArray = array_map(function($item) {
+            return str_replace('"', '', trim($item, '"'));
+        }, explode(',', trim($locations, '[]')));
+        return $locationsArray ? $locationsArray : [];
     }
 }
 ?>
